334a245ca54b1d894947d90e6f0122752cf2f299
Youssef Mohamed <110571805+y0ussefmohamed@users.noreply.github.com>
2024-12-09 11:36:48 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,53 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
 
         Scheduler scheduler = new Scheduler();
+
+        System.out.println("Choose Scheduling Algorithm:");
+        System.out.println("1. SRTF Scheduling");
+        System.out.println("2. FCAI Scheduling");
+        System.out.print("Enter your choice: ");
+        int choice = scanner.nextInt();
+
+        if (choice != 1 && choice != 2) {
+            System.out.println("Invalid choice! Exiting program.");
+            return;
+        }
 
         System.out.print("Enter number of processes: ");
         int processesCount = scanner.nextInt();
 
         List<Process> processes = new ArrayList<>();
-
         for (int i = 0; i < processesCount; i++) {
-            System.out.printf("Enter details for Process %d (Name, Arrival Time, Burst Time, Priority, Initial Quantum): ", i + 1);
-            String name = scanner.next();
-            int arrivalTime = scanner.nextInt();
-            int burstTime = scanner.nextInt();
-            int priority = scanner.nextInt();
-            int initialQuantum = scanner.nextInt();
-            processes.add(new Process(name, arrivalTime, burstTime, priority, initialQuantum));
+            if(choice == 1)
+            {
+                System.out.printf("Enter details for Process %d (Name, Arrival Time, Burst Time): ", i + 1);
+                String name = scanner.next();
+                int arrivalTime = scanner.nextInt();
+                int burstTime = scanner.nextInt();
+                processes.add(new Process(name, arrivalTime, burstTime, 0, 0));
+            }
+            else if (choice == 2)
+            {
+                System.out.printf("Enter details for Process %d (Name, Arrival Time, Burst Time, Priority, Quantum): ", i + 1);
+                String name = scanner.next();
+                int arrivalTime = scanner.nextInt();
+                int burstTime = scanner.nextInt();
+                int priority = scanner.nextInt();
+                int quantum = scanner.nextInt();
+                processes.add(new Process(name, arrivalTime, burstTime, priority, quantum));
+            }
         }
 
-        scheduler.FCAI_Scheduling(processes);
+        if (choice == 1)
+            scheduler.SRTF_Scheduling(processes);
+        else if (choice == 2)
+            scheduler.FCAI_Scheduling(processes);
+
+        scanner.close();
     }
 }
