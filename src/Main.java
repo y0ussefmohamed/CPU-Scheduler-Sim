617f19992db1f616b0d22869b497f4b65c0c41c6
Doola0501 <149879265+Doola0501@users.noreply.github.com>
2024-12-09 15:14:29 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,16 +12,20 @@ public class Main {
         System.out.println("Choose Scheduling Algorithm:");
         System.out.println("1. SRTF Scheduling");
         System.out.println("2. FCAI Scheduling");
+        System.out.println("3. SJF Scheduling");
+        System.out.println("4. Priority Scheduling");
         System.out.print("Enter your choice: ");
         int choice = scanner.nextInt();
 
-        if (choice != 1 && choice != 2) {
+        if (choice < 1 || choice > 4) {
             System.out.println("Invalid choice! Exiting program.");
-            return;
+            return; // Exit if the choice is invalid
         }
 
         System.out.print("Enter number of processes: ");
         int processesCount = scanner.nextInt();
+        System.out.print("Enter value of contextSwitching: ");
+        int contextSwitching= scanner.nextInt();
 
         List<Process> processes = new ArrayList<>();
         for (int i = 0; i < processesCount; i++) {
@@ -43,12 +47,33 @@ public class Main {
                 int quantum = scanner.nextInt();
                 processes.add(new Process(name, arrivalTime, burstTime, priority, quantum));
             }
+            else if(choice == 3)
+            {
+                System.out.printf("Enter details for Process %d (Name, Arrival Time, Burst Time): ", i + 1);
+                String name = scanner.next();
+                int arrivalTime = scanner.nextInt();
+                int burstTime = scanner.nextInt();
+                processes.add(new Process(name, arrivalTime, burstTime, 0, 0));
+            }
+            else if(choice == 4)
+            {
+                System.out.printf("Enter details for Process %d (Name, Arrival Time, Burst Time,priority): ", i + 1);
+                String name = scanner.next();
+                int arrivalTime = scanner.nextInt();
+                int burstTime = scanner.nextInt();
+                int priority = scanner.nextInt();
+                processes.add(new Process(name, arrivalTime, burstTime, priority, 0));
+            }
         }
 
         if (choice == 1)
             scheduler.SRTF_Scheduling(processes);
         else if (choice == 2)
             scheduler.FCAI_Scheduling(processes);
+        else if (choice == 3)
+            scheduler.nonPreemptiveSJF(processes);
+        else if (choice == 4)
+            scheduler.nonPreemptivePriority(processes,contextSwitching);
 
         scanner.close();
     }
